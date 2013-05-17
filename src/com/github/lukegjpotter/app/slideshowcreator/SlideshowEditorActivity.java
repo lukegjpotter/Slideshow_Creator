587b62483da78649d41bbf589d2a7fc841b12520
Luke GJ Potter <lukegjpotter@gmail.com>
2013-05-17 21:09:40 +0100

    Added imports

--- src/com/github/lukegjpotter/app/slideshowcreator/SlideshowEditorActivity.java
+++ src/com/github/lukegjpotter/app/slideshowcreator/SlideshowEditorActivity.java
@@ -1,10 +1,25 @@
 package com.github.lukegjpotter.app.slideshowcreator;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.List;
 
-public class SlideshowEditorActivity extends Activity {
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.view.Menu;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SlideshowEditorActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
