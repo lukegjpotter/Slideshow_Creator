5e1dfcf908821af80c718640044510758749962c
Luke GJ Potter <lukegjpotter@gmail.com>
2013-05-22 23:59:04 +0100

    Adding the Picture Taker Activity.

---
+++ src/com/github/lukegjpotter/app/slideshowcreator/PictureTakerActivity.java
@@ -0,0 +1,21 @@
+package com.github.lukegjpotter.app.slideshowcreator;
+
+/**
+ * @author Luke GJ Potter - lukegjpotter
+ * Date: 22/05/2013
+ *
+ * Version 1.0
+ *
+ * Description:
+ *     This is the activity for taking a picture
+ *     with the device's camera.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PictureTakerActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
