ac573afcba09d86ad9bbec216e4a2dc282a7a230
Luke GJ Potter <lukegjpotter@gmail.com>
2013-06-15 00:20:33 +0100

    Setup initial Camera packages for PictureTakerActivity.

--- src/com/github/lukegjpotter/app/slideshowcreator/PictureTakerActivity.java
+++ src/com/github/lukegjpotter/app/slideshowcreator/PictureTakerActivity.java
@@ -12,9 +12,27 @@ package com.github.lukegjpotter.app.slideshowcreator;
  */
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.List;
 
 public class PictureTakerActivity extends Activity {
+
+    // Tag for logging.
+    private static final String TAG = "PICTURE_TAKER";
+
+    private SurfaceView surfaceView;     // Used to display the camera preview.
+    private SurfaceHolder surfaceHolder; // Manages the SurfaceView changes.
+    private boolean isPreviewing;        // Is the previewing running?
+
+    private Camera camera;                                 // Used to capture image data.
+    private List <String> effects;                         // Supported colour effects for Camera.
+    private List <Camera.Size> sizes;                      // Supported preview sizes for Camera.
+    private String effect = Camera.Parameters.EFFECT_NONE; // Default effect.
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
